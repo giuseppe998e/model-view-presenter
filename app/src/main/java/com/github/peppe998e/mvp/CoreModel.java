28313dfe69e51c3cf8ee1032d6d136838b4a2d97
Giuseppe Eletto <peppe.eletto@gmail.com>
2019-10-25 13:18:07 +0200

    Update ModelCore

--- app/src/main/java/com/github/peppe998e/mvp/CoreModel.java
+++ app/src/main/java/com/github/peppe998e/mvp/CoreModel.java
@@ -2,6 +2,10 @@ package com.github.peppe998e.mvp;
 
 import com.github.peppe998e.mvp.utils.CallBack;
 
+/**
+ * Interface of CoreModel in order to hide methods and grow modularity
+ *
+ */
 public interface CoreModel {
     void testString(CallBack<String> cb);
 }
